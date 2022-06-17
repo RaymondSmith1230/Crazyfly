919776264546400215fc2fd611a05595d9d6bfff
Raymond <codingisgood598@gmail.com>
2022-06-17 10:01:52 -0500

    commit on main

---
+++ REACT/JAVASCRIPT/function.js
@@ -0,0 +1,44 @@
+
+// ReactDOM.render(<h1 className="header">Welcome To My Page</h1>, document.getElementById("root"));
+
+// const element = <h2 className="secHeander">This is coding</h2>
+// ReactDOM.render(element, document.getElementById("part2"));
+
+
+// ReactDOM.render(
+//     <div>
+//         <h1>Putting two thing's togather</h1>
+//         <p>lorem is a old way of text but we us it for coding</p>
+//     </div>,
+//     document.getElementById("text")
+// )
+
+
+// const page =
+// <div>
+//     <h2>Hello World</h2>
+//     <p>this is just a text to see if this work</p>
+// </div>
+// ReactDOM.render(page, document.getElementById("part3"))
+
+
+// ReactDOM.render(<nav>
+//     <h1>Something New</h1>
+//     <ul>
+//         <li>Pricing</li>
+//         <li>About</li>
+//         <li>Contact</li>
+//     </ul>
+// </nav>, document.getElementById("part2"));
+
+
+const topHeader =
+<nav>
+    <h1>the same new</h1>
+    <ul>
+        <li>old</li>
+        <li>new</li>
+        <li>else</li>
+    </ul>
+</nav>
+ReactDOM.render(topHeader, document.getElementById("text")); 
